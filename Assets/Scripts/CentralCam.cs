d36913cb8a7922ffb1b1214fc786ca287d977749
Gerd Schmidt <gerd@acagamics.de>
2016-09-24 22:14:12 +0200

    Better camera movement for multiroom scenes

---
+++ Assets/Scripts/CentralCam.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+[RequireComponent(typeof(Camera))]
+public class CentralCam : MonoBehaviour
+{
+    public Controller player1;
+    public Controller player2;
+    private Camera cam;
+
+    public Vector3 Offset = new Vector3(0, 5,0);
+
+	// Use this for initialization
+	void Start ()
+	{
+	    cam = this.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    var center = (player1.transform.position + player2.transform.position) / 2.0f;
+
+
+        //cam.transform.rotation = Quaternion.identity;
+	    cam.transform.position = Offset + center + new Vector3(0, 0, 0);
+
+        //cam.transform.LookAt(center);
+
+        Debug.Log(center);
+
+	}
+}
