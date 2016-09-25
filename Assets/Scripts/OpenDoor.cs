e71024731937178279a4e17993b8ab561c65bf34
Gerd Schmidt <gerd@acagamics.de>
2016-09-25 21:52:54 +0200

    Added door moving instead of simple "removing"

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -1,21 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(InfluencedObject))]
 public class OpenDoor : MonoBehaviour
 {
-    private InfluencedObject state;
+    #region member vars
 
-	// Use this for initialization
-	void Start ()
-	{
-	    state = this.gameObject.GetComponent<InfluencedObject>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-        if(state.IsActivated)
-            GameObject.Destroy(this.gameObject);
-	}
+    public float DoorMoveDistance = 3;
+    public float DoorMoveSpeed = 0.012f;
+
+    private float _doorDistance;
+    private InfluencedObject _state;
+
+    #endregion
+
+    #region methods
+
+    // Use this for initialization
+    void Start()
+    {
+        _state = gameObject.GetComponent<InfluencedObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_state.IsActivated)
+        {
+            gameObject.transform.localPosition += gameObject.transform.rotation * Vector3.forward * DoorMoveSpeed;
+            _doorDistance += DoorMoveSpeed;
+
+            if (_doorDistance > DoorMoveDistance) Destroy(gameObject);
+        }
+    }
+
+    #endregion
 }
