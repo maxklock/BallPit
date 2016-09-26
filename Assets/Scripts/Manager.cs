c51446c761ab2780db00795bf44ff0cc647fc0ca
Maximilian Klockmann <maximilian.web@gmx.net>
2016-09-26 17:06:38 +0200

    MainMenu

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -25,14 +25,20 @@ public class Manager : MonoBehaviour
     private ShakeIt _shakeIt;
 
     public Loading LoadingScreen;
+    public MainMenu MainMenu;
 
     private bool _isLoading;
+    private bool _isMenu;
 
     // Use this for initialization
     private void Start()
     {
-        _isLoading = true;
-        LoadingScreen.gameObject.SetActive(true);
+        _isMenu = true;
+        _isLoading = false;
+
+        MainMenu.gameObject.SetActive(true);
+        LoadingScreen.gameObject.SetActive(false);
+
         foreach (var player in Players)
         {
             player.gameObject.SetActive(false);
@@ -40,30 +46,58 @@ public class Manager : MonoBehaviour
 
         foreach (var room in Rooms)
         {
-            room.gameObject.SetActive(room.Game == Game);
-            if (room.Game == Game)
-            {
-                _currentRoom = room;
-            }
-        }
-        _shakeIt = _currentRoom.GetComponent<ShakeIt>();
-        if (_shakeIt != null)
-        {
-            _shakeIt.IsActive = true;
+            room.gameObject.SetActive(false);
         }
 
-        Spawner.BallSpawner.gameObject.SetActive(true);
-        Spawner.ItemSpawner.gameObject.SetActive(Game == GameType.Collect);
-        Spawner.SortSpawner.gameObject.SetActive(Game == GameType.Sort);
+        Spawner.BallSpawner.gameObject.SetActive(false);
+        Spawner.ItemSpawner.gameObject.SetActive(false);
+        Spawner.SortSpawner.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (_isMenu)
+        {
+            if (!MainMenu.IsStarting)
+            {
+                return;
+            }
+
+            Game = MainMenu.GameMode;
+            // TODO PlayerCount;
+
+            _isMenu = false;
+            _isLoading = true;
+
+            MainMenu.gameObject.SetActive(false);
+            LoadingScreen.gameObject.SetActive(true);
+
+            foreach (var room in Rooms)
+            {
+                room.gameObject.SetActive(room.Game == Game);
+                if (room.Game == Game)
+                {
+                    _currentRoom = room;
+                }
+            }
+            _shakeIt = _currentRoom.GetComponent<ShakeIt>();
+            if (_shakeIt != null)
+            {
+                _shakeIt.IsActive = true;
+            }
+
+            Spawner.BallSpawner.gameObject.SetActive(true);
+            Spawner.ItemSpawner.gameObject.SetActive(Game == GameType.Collect);
+            Spawner.SortSpawner.gameObject.SetActive(Game == GameType.Sort);
+            return;
+        }
+
         if (LoadingScreen.IsLoading)
         {
             return;
         }
+
         if (_isLoading && !LoadingScreen.IsLoading)
         {
             _isLoading = false;
---
+++ Assets/Scripts/UserInterface/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    #region member vars
+
+    public Dropdown Game;
+
+    public Slider Player;
+
+    public int PlayerCount
+    {
+        get
+        {
+            return (int)Player.value;
+        }
+    }
+
+    public GameType GameMode
+    {
+        get
+        {
+            switch (Game.value)
+            {
+                case 0:
+                    return GameType.Sort;
+                case 1:
+                    return GameType.Collect;
+                case 2:
+                    return GameType.ChangeRoom;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    #endregion
+
+    #region methods
+
+    public void StartGame()
+    {
+        IsStarting = true;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        IsStarting = false;
+        // GameMode.options = Enum.GetValues(typeof(GameType)).OfType<GameType>().Select(type => new Dropdown.OptionData(type.ToString())).ToList();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    #endregion
+
+    #region properties
+
+    public bool IsStarting { get; set; }
+
+    #endregion
+}
