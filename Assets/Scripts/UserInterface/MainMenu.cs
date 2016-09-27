83a28bb7812072155b1558a5454a5c02a3c7005d
Maximilian Klockmann <maximilian.web@gmx.net>
2016-09-27 16:16:22 +0200

    Information about GameMode in MainMenu

--- Assets/Scripts/UserInterface/MainMenu.cs
+++ Assets/Scripts/UserInterface/MainMenu.cs
@@ -12,6 +12,8 @@ public class MainMenu : MonoBehaviour
 
     public Slider Player;
 
+    public Text Information;
+
     public int PlayerCount
     {
         get
@@ -60,6 +62,19 @@ public class MainMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7))
         {
             Exit();
+        }
+
+        switch (GameMode)
+        {
+            case GameType.Sort:
+                Information.text = "You have to sort balls of your color to the corner of your color.";
+                break;
+            case GameType.Collect:
+                Information.text = "You have to collect more green balls than your opponent.";
+                break;
+            case GameType.ChangeRoom:
+                Information.text = "You have to collect more green balls than your opponent. To change the room, you have to activate both pressure plates.";
+                break;
         }
     }
 
