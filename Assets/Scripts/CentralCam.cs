da4e72a710038b7aa91856bb6bdb50c459585890
Maximilian Klockmann <maximilian.web@gmx.net>
2016-09-27 17:06:50 +0200

    Better camera movement

--- Assets/Scripts/CentralCam.cs
+++ Assets/Scripts/CentralCam.cs
@@ -5,28 +5,45 @@ using System.Runtime.InteropServices;
 [RequireComponent(typeof(Camera))]
 public class CentralCam : MonoBehaviour
 {
-    public Controller player1;
-    public Controller player2;
-    private Camera cam;
+    public Controller Player1;
+    public Controller Player2;
 
-    public Vector3 Offset = new Vector3(0, 5,0);
+    public float MaxXDistance = 8;
+    public float MaxZDistance = 6;
+
+    private Camera _cam;
+
+    public Vector3 Offset;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    cam = this.GetComponent<Camera>();
+        _cam = this.GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+	    if (!Player1.isActiveAndEnabled)
+	    {
+	        _cam.transform.position = Player2.transform.position + Offset;
+	        return;
+        }
+        if (!Player2.isActiveAndEnabled)
+        {
+            _cam.transform.position = Player1.transform.position + Offset;
+            return;
+        }
 
-	    var center = (player1.transform.position + player2.transform.position) / 2.0f;
+        var center = (Player1.transform.position + Player2.transform.position) / 2.0f;
+	    var z = Mathf.Min(Player1.transform.position.z, Player2.transform.position.z);
+	    var distX = Mathf.Max(0, Mathf.Abs(Player1.transform.position.x - Player2.transform.position.x) - MaxXDistance);
+        var distZ = Mathf.Max(0, Mathf.Abs(Player1.transform.position.z - Player2.transform.position.z) - MaxZDistance);
 
-
-        //cam.transform.rotation = Quaternion.identity;
-	    cam.transform.position = Offset + center + new Vector3(0, 0, 0);
-
+        center.z = z - distX;
+        center.y += Mathf.Max(distX, distZ);
+        
+        _cam.transform.position = Offset + center;
 
 	}
 }
