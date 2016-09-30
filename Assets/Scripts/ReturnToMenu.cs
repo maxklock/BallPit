02836df3e2f00800796dc757e6c2819f004df3e7
Gerd Schmidt <gerd@acagamics.de>
2016-09-30 09:54:28 +0200

    Return to Menu while playing

---
+++ Assets/Scripts/ReturnToMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spawner;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour {
+
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7))
+        {
+            BallSpawner.BallCount = 0;
+            ItemSpawner.ItemCount = 0;
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+    }
+}
